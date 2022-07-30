d3e0bb27bec3fab088830c3b09b37947c109ab28
KnJbMfLAgdkwZL <zipocat@gmail.com>
2022-07-30 21:45:55 +0300

    fix model book

--- Database/Models/Book.cs
+++ Database/Models/Book.cs
@@ -5,12 +5,12 @@ namespace Database.Models;
 public sealed class Book : IEntity
 {
     public int Id { get; set; }
-    public string Title { get; set; } = null!;
-    public string Cover { get; set; } = null!;
-    public string Content { get; set; } = null!;
-    public string Author { get; set; } = null!;
-    public string Genre { get; set; } = null!;
+    public string Title { get; set; } = "";
+    public string Cover { get; set; } = "";
+    public string Content { get; set; } = "";
+    public string Author { get; set; } = "";
+    public string Genre { get; set; } = "";
 
-    public List<Rating> Ratings { get; set; } = null!;
-    public List<Review> Reviews { get; set; } = null!;
+    public List<Rating> Ratings { get; set; } = new();
+    public List<Review> Reviews { get; set; } = new();
 }
